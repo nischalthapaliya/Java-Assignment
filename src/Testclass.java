617dba6b6381d592a63254ed8ba899341527c29c
Nischal Thapaliya <nischalthapaliya0723@gmail.com>
2020-05-19 09:14:24 +0545

    Answer No.5

---
+++ src/Testclass.java
@@ -0,0 +1,11 @@
+public class Testclass {
+    static int sum() {
+        int x = 5, y = 6, sum;
+        return sum=(x+y);
+    }
+
+    public static void main(String[] args) {
+        Testclass myObj = new Testclass();
+        System.out.println(myObj.sum());
+    }
+}
